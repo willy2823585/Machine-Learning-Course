3941b1f50fd45854b7e703362b09a32721dadc7d
willy2823585 <willy282358528@gmail.com>
2019-10-11 16:37:54 +0800

    第一次作業檔

---
+++ Deep Learning/Homework1/hw1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 11 16:34:00 2019
+
+@author: willy
+"""
+
+import tensorflow 
+import keras 
---
+++ Deep Learning/Homework1/hw2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 11 16:36:18 2019
+
+@author: willy
+"""
+
