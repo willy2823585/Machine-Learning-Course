d275e091d5a299130691bfb41bb77e0a52aeb16a
willy2823585 <willy282358528@gmail.com>
2019-10-22 21:39:22 +0800

    Use Test Data  to validation

--- Deep Learning/Homework2/Hw1.py
+++ Deep Learning/Homework2/Hw1.py
@@ -4,6 +4,7 @@ Created on Tue Oct 22 15:59:12 2019
 
 @author: willywanghw
 """
+#7108029211 王皓威
 
 import keras
 keras.__version__
@@ -23,15 +24,9 @@ from keras import models
 from keras import layers
 
 network = models.Sequential()
-#參數修改  試過sigmoid,relu..等
 network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
-#層數加入  準確度0.9752   神經元512>>1024
 network.add(layers.Dense(512, activation='sigmoid'))
-#參數修改 
 network.add(layers.Dense(10,activation='softmax'))
-
-#Keras優化器的公共參數 RMSprop,SGD,Adagrad,Adadelta,Adam
-#adam 約98%  Adadelta 0.98  Adagrad 約0.98  SGD 約0.9246   RMSprop  0.9781
 
 network.compile(optimizer='rmsprop',
                 loss='categorical_crossentropy',
@@ -47,18 +42,16 @@ from keras.utils import to_categorical
 
 train_labels = to_categorical(train_labels)
 test_labels = to_categorical(test_labels)
-#batch_size =128 改成64
-networkht = network.fit(train_images, train_labels, epochs=25, batch_size=256,validation_split = 0.2, verbose=1)
+
+#測試集放進驗證集
+networkht = network.fit(train_images, train_labels, epochs=25, batch_size=256,validation_data=(test_images,test_labels), verbose=1)
 net_dict = networkht.history
 test_loss, test_acc = network.evaluate(test_images, test_labels)
-
-print('test_acc:', test_acc)
-
 import matplotlib.pyplot as plt 
 loss = networkht.history['loss']
 val_loss = networkht.history['val_loss']
 epochs = range(1,len(loss)+1)
-plt.plot(epochs,loss,'ro',label='Training loss')
+plt.plot(epochs,loss,'r',label='Training loss')
 plt.plot(epochs,val_loss,'b',label = 'validation loss')
 plt.title('Training and validataion loss')
 plt.xlabel('Epochs')
@@ -79,3 +72,4 @@ plt.ylabel('Accuracy')
 plt.legend()
 plt.show()
 
+print('test_acc:', test_acc)
